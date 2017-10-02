f5e36e337e5e62631cd7aef9741eb739f1154472
pechaavrriaa <pechaavrriaa@gmail.com>
2017-10-01 22:10:01 -0600

    Calculator with comments and name changed

---
+++ Calculator.cpp
@@ -0,0 +1,140 @@
+//  ========================================================================== 
+// | Assignment One : FRACTION
+// |
+// | Author : PABLO ECHAVARRIA
+// | Language : C PLUS PLUS OBJECT ORIENTED
+// | To Compile : EXPLAIN HOW TO COMPILE THIS PROGRAM
+// | Instructor : MUSTAKIM AL HELAL
+// | Due Date : SUNDAY, OCTOBER, 1, 2017 AT 11:59PM
+//  +--------------------------------------------------------------------------
+// | Description :         Write a program to model a simple calculator. Each data line 
+// |			   should consist of the next operation to be performed from the 
+// |			   list below and the right operand. Assume the left operand is 
+// |			   the accumulator value (initial value of 0). You need a function
+// |			   scan_data with two output parameters that returns the operator
+// |			   and right operand scanned from a data line. You need a function
+// |			   do_next_op that performs the required operation. do_next_op has 
+// |			   two input parameters (the operator and operand) and one
+// |			   input/output parameter (the accumulator). 
+// |			   The valid operators are:
+// |			   +	add
+// |			   -	subtract
+// |			   * 	multiply
+// |			   /	divide
+// |			   ^	power(raise left operand to power of right operand)
+// |
+// | Input : + 10
+// |         / 0
+// |         / 2.0
+// |         & 2
+// |         ^ 2
+// |         q 0
+// | 
+// | Output : Result so far is 10.0
+// |          Result so far is 10.0
+// |          Result so far is 5.0
+// |          Result so far is 5.0
+// |          Result so far is 25.0
+// |          Final result is 25.0
+// |
+//  ============================================================================
+
+//headers
+#include <iostream>// I/O library (cin, cout)
+#include <iomanip> // I/O manipulation (fixed, setprecision())
+using namespace std;// avoid ::std for cin and cout
+
+//consts
+const float EPS = 0.0000001;// constant to compare float values
+
+//functions prototypes
+void scan_data(char &, float &);//function designed to read the input
+void do_next_op(char &, float &, float &);//function to process data
+int ipow(int, int);//power function
+float Abs(float);//absolute value
+
+int main()
+{
+  char symbol = 0;//read symbol variable
+  float number = 0;//read number variable
+  float accumulatedNumber = 0;//storage result variable
+
+  cout << fixed;//all values to have fixed decimal points
+
+  while (symbol != 'q')//exit when user press 'q'
+    {
+      scan_data(symbol, number);//process info
+
+      if (symbol != 'q')//when user input 'q' there is no need to process or print data
+	{
+	  do_next_op(symbol, number, accumulatedNumber);//make operation 
+	  cout << "result so far is " << setprecision(1) << accumulatedNumber << endl;//print accumulates result
+	}
+    }
+
+  cout << "final result is " << setprecision(2) << accumulatedNumber << endl;//print final result
+}
+//function implementation
+
+//read input
+void scan_data(char & operant, float & operand)
+{
+  cin >> operant >> operand;//read symbol and number
+}
+
+//process data
+void do_next_op(char & operant, float & operand, float & accumulated)
+{
+
+  /*
+    process depending on each
+    mathematical operation
+  */
+  if (operant == '+')
+    accumulated += operand;
+
+  else if (operant == '-')
+    accumulated -= operand;
+
+  else if (operant == '*')
+    accumulated *= operand;
+
+  else if (operant == '/')
+    {
+      if (Abs(operand - 0.0) < EPS)//make sure not to do an invalid operation
+	cout << "Error: Division by 0" << endl;
+      else
+	accumulated /= operand;
+    }
+
+  else if (operant == '^')
+    accumulated = ipow(accumulated, operand);//calculate power using binary exponentiation with custom function
+  else
+    cout<<"Error: invalid operand\n";
+}
+
+//power function
+int ipow(int base, int exp)// power function, binary exponentiation used to do faster calculations
+{
+  int result = 1;//number to be returned
+  while (exp>0)//continue until exp is 0
+    {
+      if (exp & 1)//elevate when the number is odd
+	result *= base;
+      exp /= 2;//divide exp by two
+      base *= base;//base multiplied by itself
+    }
+
+  return result;//return value
+
+  //more info on https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+}
+
+//absolute value function
+float Abs(float a) 
+{
+  if (a < EPS)// if a less than 0 return -a to be positive, else return same value
+    return -a;// -a will return positive, e.g -(-5.5) = 5.5
+  else
+    return a;
+}
--- fraction.cpp
+++ fraction.cpp
@@ -26,8 +26,7 @@
 // |		  Invalid operation!
 // |		  The result of the multiplication is 6/56
 // |		  Cannot solve this problem!
-// | Algorithm : OUTLINE THE APPROACH USED BY THE PROGRAM TO SOLVE THE
-// | PROBLEM.
+// |
 // | Required Features Not Included :
 // | Known Bugs :
 //  ============================================================================
@@ -45,104 +44,108 @@ void division(int &, int &, int &, int &, int &, int &);
 
 void lowerCase(string & answer)
 {
-	for (int index = 0; index < answer.size(); index++)
-	{
-		if (answer[index] >= 'A' && answer[index] <= 'Z')
-			answer[index] = 'a' + answer[index] - 'A';
-	}
+  for (int index = 0; index < answer.size(); index++)
+    {
+      if (answer[index] >= 'A' && answer[index] <= 'Z')
+	answer[index] = 'a' + answer[index] - 'A';
+    }
 }
 
 int gcdr(int a, int b)
 {
-	if (a == 0) return b;
-	return gcdr(b % a, a);
+  if (a == 0) return b;
+  return gcdr(b % a, a);
 }
 
-void main()
+int  main()
 {
-	string continueProgram;
-	bool nextOperation = true;
-	int leftTop = 0, leftBottom = 0, rightTop = 0, rightBottom = 0, numeratorResult = 0, denominatorResult = 0;
-	char operation = 0;
-	do
+  string continueProgram;
+  bool nextOperation = true;
+  int leftTop = 0, leftBottom = 0, rightTop = 0, rightBottom = 0, numeratorResult = 0, denominatorResult = 0;
+  char operation = 0;
+  do
+    {
+      scan_data(leftTop, leftBottom, operation, rightTop, rightBottom);
+      do_next_op(leftTop, leftBottom, operation, rightTop, rightBottom, numeratorResult, denominatorResult);
+      if (denominatorResult == 0) {
+	cout << "Error division by 0\nCannot solve this problem!\n";
+      }
+      else if(denominatorResult != -1)
 	{
-		scan_data(leftTop, leftBottom, operation, rightTop, rightBottom);
-		do_next_op(leftTop, leftBottom, operation, rightTop, rightBottom, numeratorResult, denominatorResult);
-		if (denominatorResult == 0) {
-			cout << "Error division by 0\n";
-		}
-		else if(denominatorResult != -1)
-		{
-			int gcd = 0;
-			gcd = gcdr(numeratorResult, denominatorResult);
-			cout << numeratorResult / gcd << '/' << denominatorResult / gcd << '\n';
-		}
-		bool readOk = true;
-		while (readOk)
-		{
-			cout << "Continue: Yes/No?\n";
-			cin >> continueProgram;
-			lowerCase(continueProgram);
-			if (continueProgram.compare("yes") == 0)
-			{
-				nextOperation = true;
-				readOk = false;
-			}
-			else if (continueProgram.compare("no") == 0)
-			{
-				nextOperation = false;
-				readOk = false;
-			}
-			else
-				cout << "That is not an answer\n"; // I don't now what happens here when you dont use yes or no, just another word.
-		}
-	} while (nextOperation);
+	  int gcd = 0;
+	  gcd = gcdr(numeratorResult, denominatorResult);
+	  cout << numeratorResult / gcd << '/' << denominatorResult / gcd << '\n';
+	}
+      bool readOk = true;
+      while (readOk)
+	{
+	  cout << "Continue: Yes/No?\n";
+	  cin >> continueProgram;
+	  lowerCase(continueProgram);
+	  if (continueProgram.compare("yes") == 0)
+	    {
+	      nextOperation = true;
+	      readOk = false;
+	      cout<<endl;
+	    }
+	  else if (continueProgram.compare("no") == 0)
+	    {
+	      nextOperation = false;
+	      readOk = false;
+	    }
+	  else
+	    cout << "That is not an answer\n"; // I don't now what happens here when you dont use yes or no, just another word.
+	}
+    } while (nextOperation);
+
+  return 0;
 }
 
 void scan_data(int & leftNumerator, int & leftDenominator, char & operant, int & rightNumerator, int & rightDenominator)
 {
-	char fractionCharacter = 0;
-	cin >> leftNumerator >> fractionCharacter >> leftDenominator >> operant >> rightNumerator >> fractionCharacter >> rightDenominator;
+  char fractionCharacter = 0;
+  cin >> leftNumerator >> fractionCharacter >> leftDenominator >> operant >> rightNumerator >> fractionCharacter >> rightDenominator;
 
 }
 
 void do_next_op(int & leftNumerator, int & leftDenominator, char & operant, int & rightNumerator, int & rightDenominator, int & numerator, int & denominator)
 {
-	if (operant == '+')
-		addition(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
-	else if (operant == '-')
-		subtraction(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
-	else if (operant == '*')
-		multiplication(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
-	else if (operant == '/')
-		division(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
-	else
-	{
-		cout << "The operations available are \"+\", \"-\", \"*\", \"/\"\n";
-		denominator = -1;
-	}
+  if (operant == '+')
+    addition(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
+  else if (operant == '-')
+    subtraction(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
+  else if (operant == '*')
+    multiplication(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
+  else if (operant == '/')
+    division(leftNumerator, leftDenominator, rightNumerator, rightDenominator, numerator, denominator);
+  else
+    {
+      cout << "Invalid operation!\n";
+      cout << "The operations available are \"+\", \"-\", \"*\", \"/\"\n";
+      denominator = -1;
+    }
 }
 
 void addition(int & leftNumerator, int & leftDenominator, int & rightNumerator, int & rightDenominator, int & numerator, int & denominator)
 {
-	denominator = leftDenominator * rightDenominator;
-	numerator = (rightDenominator*leftNumerator) + (leftDenominator*rightNumerator);
+  denominator = leftDenominator * rightDenominator;
+  numerator = (rightDenominator*leftNumerator) + (leftDenominator*rightNumerator);
 }
 
 void subtraction(int & leftNumerator, int & leftDenominator, int & rightNumerator, int & rightDenominator, int & numerator, int & denominator)
 {
-	denominator = leftDenominator * rightDenominator;
-	numerator = (rightDenominator*leftNumerator) - (leftDenominator*rightNumerator);
+  denominator = leftDenominator * rightDenominator;
+  numerator = (rightDenominator*leftNumerator) - (leftDenominator*rightNumerator);
 }
 
 void multiplication(int & leftNumerator, int & leftDenominator, int & rightNumerator, int & rightDenominator, int & numerator, int & denominator)
 {
-	denominator = leftDenominator * rightDenominator;
-	numerator = leftNumerator * rightNumerator;
+  denominator = leftDenominator * rightDenominator;
+  numerator = leftNumerator * rightNumerator;
 }
 
 void division(int & leftNumerator, int & leftDenominator, int & rightNumerator, int & rightDenominator, int & numerator, int & denominator)
 {
-	denominator = leftDenominator * rightNumerator;
-	numerator = leftNumerator * rightDenominator;
+  denominator = leftDenominator * rightNumerator;
+  numerator = leftNumerator * rightDenominator;
 }
