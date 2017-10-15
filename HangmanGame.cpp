f88ca6755aab0ae550ec66c2bf1c1eba2c0b17f5
juanea <juanea@gmail.com>
2017-10-15 01:07:53 -0600

    Update and rename Hangman.cpp to HangmanGame.cpp

--- HangmanGame.cpp
+++ HangmanGame.cpp
@@ -13,6 +13,21 @@ protected:
 	ifstream wordsFile;//ifstream to work with file of words
 	int numOfWords;//know the number of words to later generate random word
 };
+class randNumbers :public fStreaming
+{
+public:
+	void getWord();//randomly get a word
+protected:
+	string word;//store the word
+};
+class process : public randNumbers
+{
+public:
+	void playWithUser();//simulation of game
+	process(string gameLevel);//ask difficulty of game and create variable
+protected:
+	int attempts;//num of attempts left
+};
 //Open file implementation, where the words are stored
 void fStreaming::openFile()
 {
@@ -33,28 +48,23 @@ void fStreaming::countWords()
 		numOfWords++; //Count number of words
 	}
 }
-class randNumbers :public fStreaming
-{
-public:
-	void getWord();//randomly get a word
-protected:
-	string word;//store the word
-};
-
+//get word from file
 void randNumbers::getWord()
 {
-
+	srand(time(NULL));//Initialize random seed
+	int randWord = 0;
+	randWord = rand() % numOfWords;//Getting a random index of the words list
+	int index = 0;
+	string temp;
+	while (wordsFile >> temp)
+	{
+		if (index == randWord)//FInd random word index
+		{
+			word = temp;
+		}
+		index++;
+	}
 }
-class process : public randNumbers
-{
-public:
-	void playWithUser();//simulation of game
-	process(string gameLevel);//ask difficulty of game and create variable
-protected:
-	int attempts;//num of attempts left
-};
 int main() {
-
-
 	return 0;
 }
