693c3a161f1c955a446458306749d05dc1cbbcb0
Juan Echavarria <juanea@gmail.com>
2017-10-27 16:28:05 -0600

    Update

--- Calendar.cpp
+++ Calendar.cpp
@@ -37,56 +37,17 @@
 //// |
 ////  ============================================================================
 
-//headers
-#include <iostream>// I/O library (cin, cout)
-#include <iomanip> // I/O manipulation (fixed, setprecision())
+#include "Calendar.h"
 
-using namespace std;// avoid ::std for cin and cout
-
-class DateInitialization
-{
-public:
-		DateInitialization();
-		void stringToInt(int &, int &, int &);
-
-protected:
-	string date;
-};
-
-class DateToInt : public DateInitialization
-{
-public:
-	DateToInt(int, int, int);
-	int day;
-	int month;
-	int year;
-};
 int main()
 {
 	int month;
 	int day;
-	int year=0;
+	int year = 0;
+	string date;
 
 	DateInitialization dateInitalization;
 	dateInitalization.stringToInt(month, day, year);
-	DateToInt dateToInt( month,  day, year);
-
-}
-void  DateInitialization::stringToInt(int& month, int& day, int& year)
-{
-	month = (date[0] - '0') * 10 + (date[1] - '0');
-	day = (date[3] - '0') * 10 + (date[4] - '0');
-	for (int i = 6; i < 10; i++)
-		year = year * 10 + (date[i] - '0');
-
-}
-DateToInt::DateToInt(int m, int d, int y)
-{
-	day = d;
-	month = m;
-	year = y;
-}
-DateInitialization::DateInitialization(void)
-{
-	date = "01-01-1900";
+	DateToInt dateToInt(month, day, year);
+	cin >> date; //Reading string of date of format mm-dd-yyyy
 }
--- Calendar.h
+++ Calendar.h
@@ -1,1 +1,29 @@
 #pragma once
+
+//headers
+#include <iostream>// I/O library (cin, cout)
+#include <iomanip> // I/O manipulation (fixed, setprecision())
+#include <string> // String library for manipulating strings of any character type.
+
+using namespace std;// avoid ::std for cin and cout
+
+class DateInitialization // Class for initalizing the first day of the calendar in the programs as default.
+{
+public:
+	DateInitialization();
+	void stringToInt(int &, int &, int &);
+
+
+protected:
+	string date;
+};
+
+class DateToInt : public DateInitialization //Funtion for setting the first day before the user input any day
+{
+public:
+	DateToInt(int, int, int); // Function for fonverting the date (string) into integers.
+	int day;
+	int month;
+	int year;
+};
+
