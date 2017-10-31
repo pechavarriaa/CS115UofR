15482c20875d24495882cc47898584b00a9fc2b2
Juan Echavarria <juanea@gmail.com>
2017-10-31 17:11:02 -0600

    Comenting code

--- Calendar.cpp
+++ Calendar.cpp
@@ -1,73 +1,69 @@
-////  ========================================================================== 
-//// | Assignment Three : CALENDAR PROJECT
-//// |
-//// | Author : PABLO ECHAVARRIA
-//// | Language : C++
-//// | Instructor : MUSTAKIM AL HELAL
-//// | Due Date : WEDNESDAY, NOVEMBER, 1, 2017 AT 11:59PM
-////  +--------------------------------------------------------------------------
-//// | Description :  Develop in C++ a class date to represent a calendar.The class should provide the following
-//// |				operations :
-//// |							-A default constructor that initializes a date object to 01 - 01 - 1900.
-//// |							-A class constructor that initializes a date object to a correct value 
-//// |							 using three integer parameters corresponding to the desired month, day and year.
-//// |							 The function toString() that returns the string version of a date object.For example,
-//// |							 applying toString() to the date 12 - 01 - 2000 produces "December 1st, 2000".
-//// |							 The function nextDate() that returns the successive date i.e.the new value of the
-//// |							 date object.For example, applying nextDate() to the date 12 - 31 - 2000 produces a
-//// |							 new date: 01 - 01 - 2001. You should take into account if the year is a leap year or not.
-//// |							 A leap year is : (1) divisible by 400 or (2) divisible by 4 and not divisible by 100.
-//// |							 The function compareDates() that checks if the date of interest is before, after or
-//// |							 equal to the argument date.
-//// |
-//// |			A simple run of the driver program follows.
-//// |		Enter the first date using the format mm - dd - yyyy: 12-32-2000
-//// |		Incorrect day!
-//// |		Enter the first date using the format mm - dd - yyyy : 12-31-2000
-//// |		The string version of the date is : December 31st, 2000
-//// |		The next date in string version is : January 1st, 2001
-//// |		Enter the second date using the format mm - dd - yyyy : 12-01-2001
-//// |		The first date comes before the second one.
-//// |		Another run :
-//// |		Enter the first date using the format mm - dd - yyyy : 02-28-2005
-//// |		The string version of the date is : February 28th, 2005
-//// |		The next date in string version is : March 1st, 2005
-//// |		Enter the second date using the format mm - dd - yyyy : 01-10-2005
-//// |		The first date comes after the second one.
-//// |
-////  ============================================================================
+/*//  ==========================================================================
+// | Assignment Three : CALENDAR PROJECT
+// |
+// | Author : PABLO ECHAVARRIA
+// | Language : C++
+// | Instructor : MUSTAKIM AL HELAL
+// | Due Date : WEDNESDAY, NOVEMBER, 1, 2017 AT 11:59PM
+//  +--------------------------------------------------------------------------
+// | Description :  Develop in C++ a class date to represent a calendar.The class should provide the following
+// |				operations :
+// |							-A default constructor that initializes a date object to 01 - 01 - 1900.
+// |							-A class constructor that initializes a date object to a correct value
+// |							 using three integer parameters corresponding to the desired month, day and year.
+// |							 The function toString() that returns the string version of a date object.For example,
+// |							 applying toString() to the date 12 - 01 - 2000 produces "December 1st, 2000".
+// |							 The function nextDate() that returns the successive date i.e.the new value of the
+// |							 date object.For example, applying nextDate() to the date 12 - 31 - 2000 produces a
+// |							 new date: 01 - 01 - 2001. You should take into account if the year is a leap year or not.
+// |							 A leap year is : (1) divisible by 400 or (2) divisible by 4 and not divisible by 100.
+// |							 The function compareDates() that checks if the date of interest is before, after or
+// |							 equal to the argument date.
+// |
+// |			A simple run of the driver program follows.
+// |		Enter the first date using the format mm - dd - yyyy: 12-32-2000
+// |		Incorrect day!
+// |		Enter the first date using the format mm - dd - yyyy : 12-31-2000
+// |		The string version of the date is : December 31st, 2000
+// |		The next date in string version is : January 1st, 2001
+// |		Enter the second date using the format mm - dd - yyyy : 12-01-2001
+// |		The first date comes before the second one.
+// |		Another run :
+// |		Enter the first date using the format mm - dd - yyyy : 02-28-2005
+// |		The string version of the date is : February 28th, 2005
+// |		The next date in string version is : March 1st, 2005
+// |		Enter the second date using the format mm - dd - yyyy : 01-10-2005
+// |		The first date comes after the second one.
+// |
+//  ============================================================================*/
+
 // http://www.cs.uregina.ca/Links/class-info/115/08-overloading/
-#include "Calendar.h"
+
+#include "Calendar.h" // Header for including headers and function prototypes
 
 int main()
 {
-	int month = 0;
-	int day = 0;
-	int year = 0;
-	string date;
+	int month = 0;	//Declaration of month for the object constructor in the function toString()
+	int day = 0;	//Declaration of day for the object constructor in the function toString()
+	int year = 0;	//Declaration of year for the object constructor in the function toString()
+	string date;	//Declaration of the string date for inputing into the function processInput()
 
+	// First date
 	FullDate dateObj(month, day, year);	//Created for 01-01-1900 running two constructors
-	dateObj.stringToInt(); //Passing values from the string date converting them to three variables int for manipulation
-	cout << "Enter the first date using the format mm-dd-yyyy : " << endl;
-	getline(cin, date); //Reading string of date of format mm-dd-yyyy
-
-	
-	processInput(date, dateObj,true);
-
+	dateObj.stringToInt();	//Passing values from the string date converting them to three variables int for manipulation
+	cout << "Enter the first date using the format mm-dd-yyyy : ";	//Console user instructions
+	getline(cin, date);	//Reading string of date of format mm-dd-yyyy
+	processInput(date, dateObj, true);	//Function for checking that the user gave the correct information in the same format as asked
 	cout << "The string version of the date is : ";
-	cout << dateObj.toString() << endl;
-
+	cout << dateObj.toString() << endl;	//Printing the date with the name of the month and the ordinal number
 	cout << "The next date in string version is :";
-	FullDate nextDay = dateObj.nextDate();
-	cout << nextDay.toString() << endl;
+	FullDate nextDay = dateObj.nextDate();	//Generating the next day from the one that the user input
+	cout << nextDay.toString() << endl;	//Printing the next day from the one that the user input
 
+	// Second date
 	cout << "Enter the second date using the format mm-dd-yyyy : ";
 	FullDate secondDateObj(month, day, year);
-
 	getline(cin, date); //Reading string of date of format mm-dd-yyyy
-
-	processInput(date, secondDateObj,false);
-
-	cout << dateObj.compareDates(secondDateObj) << endl;
-
+	processInput(date, secondDateObj, false);	//Function for checking that the user gave the correct information in the same format as asked
+	cout << dateObj.compareDates(secondDateObj) << endl;	//Funtion for comparing the dates, it tells the user which one goes after the other one
 }
--- TestCalendar.cpp
+++ TestCalendar.cpp
@@ -1,51 +1,64 @@
-#include "Calendar.h"
+#include "Calendar.h"	// Header for including headers and function prototypes
 
+
+// Class constructor for initilizing a date object
 FullDate::FullDate(int m, int d, int y)
 {
 	day = d;
 	month = m;
 	year = y;
 }
+
+// Function for converting the string date into three integers for the month day and the year
 void  FullDate::stringToInt()
 {
-	month = year = day = 0; //To clear previos date
-	month = (date[0] - '0') * 10 + (date[1] - '0');
-	day = (date[3] - '0') * 10 + (date[4] - '0');
+	month = year = day = 0; // To clear previos date
+	month = (date[0] - '0') * 10 + (date[1] - '0');	//For getting the month from the string into the integer month
+	day = (date[3] - '0') * 10 + (date[4] - '0');	//For getting the day from the string into the integer day
 	for (int i = 6; i < date.size(); i++)
-		year = year * 10 + (date[i] - '0');
-
+		year = year * 10 + (date[i] - '0');	//For getting the year from the string into the integer year
 }
+
+//	Funtion for checking if the date belongs into date in the gregorian calendar
 bool FullDate::check()
 {
-	if (month > 12)
+	const int numOfMonths = 12;
+	if (month > numOfMonths) //Checking if the month that is inputed is not more than 12
 		return false;
-	if (day > endOfMonth())
+	if (day > endOfMonth()) //Checks that the day inputed do not exceed between the end of each month
 		return false;
-	return true;
+	return true; //If the day is in the boundaries the function returns true;
 }
-void FullDate::intToString()
-{
-	string m;
-	if (month < 10)
-		m = "0" + to_string(month);
-	else
-		m = to_string(month);
 
-	string d;
-	if (day < 10)
-		d = "0" + to_string(day);
-	else
-		d = to_string(day);
+////	Function for converting the date into the string version of 
+//void FullDate::intToString()
+//{
+//	string m;
+//	if (month < 10)
+//		m = "0" + to_string(month);
+//	else
+//		m = to_string(month);
+//
+//	string d;
+//	if (day < 10)
+//		d = "0" + to_string(day);
+//	else
+//		d = to_string(day);
+//
+//	date = m + "-" + d + "-" + to_string(year);
+//}
 
-	date = m + "-" + d + "-" + to_string(year);
-}
+//	Function for converting the integers values of month, day and year into string version with name of the month and the ordinal number of the day
 string FullDate::toString()
 {
+	// Name of the months stored in a string array
 	const string monthName[] = { "January", "February", "March", "April", "May", "June",
 								 "July", "August", "September", "October", "November", "December" };
 
-	string ordinalNumbers[40] = { "1st", "2nd", "3rd" };
-	for (int i = 3; i < 30; i++)
+	// Ordinal nubers stored in an string array
+	const int maxDay = 31; // Maximum day that some of the months have
+	string ordinalNumbers[maxDay] = { "1st", "2nd", "3rd" }; //First three numbers stored in the string array
+	for (int i = 3; i < 30; i++) // For loop for storing the numbers that finish in th
 	{
 		if (i + 1 > 20 && i + 1 < 24)
 			continue;
@@ -55,12 +68,15 @@ string FullDate::toString()
 	ordinalNumbers[30] = "22nd";
 	ordinalNumbers[30] = "23rd";
 	ordinalNumbers[30] = "31st";
-	string returnedDate = "";
-	returnedDate = monthName[month - 1] + " " + ordinalNumbers[day - 1] + ", " + to_string(year);
-	return returnedDate;
+	string returnedDate = ""; // String declaration
+	returnedDate = monthName[month - 1] + " " + ordinalNumbers[day - 1] + ", " + to_string(year); // String concatenation for result
+	return returnedDate; // Returning the date with month name and ordinal number type as a string
 }
+
+// Function that checks each end of the month 
 int FullDate::endOfMonth()
 {
+	//Last day of each month
 	//January - 31 days
 	//February - 28 days in a common year and 29 days in leap years
 	//March - 31 days
@@ -74,42 +90,44 @@ int FullDate::endOfMonth()
 	//November - 30 days
 	//December - 31 days
 
+	const int finalDate[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //Declaration of last day of the month as a constant array
 
-	const int finalDate[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-	if (month != 2)
+	if (month != 2) //Checking if is not February
 		return finalDate[month - 1];
 	else
 	{
-		if (year % 400 == 0 || (year % 4 == 0 && year % 4 != 0))
-			return 29;
+		if (year % 400 == 0 || (year % 4 == 0 && year % 4 != 0)) //If it is February checks if the year is leap or not.
+			return 29;	//Leap year
 		else
-			return 28;
+			return 28; //Not a leap year
 	}
 }
+
+//Operator overloading, for adding one day to the existing one
 FullDate FullDate::operator ++ (int)
 {
 	if (day == endOfMonth())  //endOfMonth returns 28, 29, 30 or 31 depending on month 
 	{
-		if (month == 12)
+		if (month == 12) //If it is the last month of the year
 		{
 			year += 1;
 			month = 1;
 		}
-		else
-			month += 1;
-		day = 1;
+		else 
+			month += 1; //If it is not it just adds one month to the existing one
+		day = 1; //And it start in the first day of the next month
 	}
 	else
-		day += 1;
+		day += 1; //If is not on the end of the month nor the year it just adds one day to the existing day
 	return *this;
 }
+
+//
 FullDate FullDate::nextDate()
 {
-	FullDate temp(month, day, year);
-	temp++;
+	FullDate temp(month, day, year); //Declaring temporal of the class FullDate
+	temp++; //Operator overloading for the next day
 	return temp;
-
 }
 string FullDate::compareDates(FullDate secondDate)
 {
@@ -149,7 +167,6 @@ void DateInitialization::setDate(string userDate)
 {
 	date = userDate;
 }
-
 bool checkInput(string date, FullDate &dateObj)
 {
 	bool ck = false; //check input
@@ -165,7 +182,7 @@ bool checkInput(string date, FullDate &dateObj)
 			if (date[i] != '-')
 				ck = true;
 		}
-	
+
 	}
 	if (ck == false)
 	{
@@ -176,16 +193,16 @@ bool checkInput(string date, FullDate &dateObj)
 	}
 	return !ck;
 }
-void processInput(string date, FullDate &dateObj,bool firstOrSecond)
+void processInput(string date, FullDate &dateObj, bool firstOrSecond)
 {
 
 	while (checkInput(date, dateObj) == false)
 	{
 		cout << "Incorrect day!" << endl;
-		if(firstOrSecond)
-		cout << "Enter the first date using the format mm-dd-yyyy: " << endl;
+		if (firstOrSecond)
+			cout << "Enter the first date using the format mm-dd-yyyy: ";
 		else
-		cout << "Enter the second date using the format mm-dd-yyyy: " << endl;
+			cout << "Enter the second date using the format mm-dd-yyyy: ";
 		getline(cin, date); //Reading string of date of format mm-dd-yyyy
 	}
 }
